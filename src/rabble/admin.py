25b590e35fae57aeb3c83735a085eac40746d6e6
Quincy Leung <quincyleung@uchicago.edu>
2025-04-17 18:35:47 -0500

    Added models in admin site for HW#2

--- src/rabble/admin.py
+++ src/rabble/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Community, CommunityMember, SubRabble, SubRabbleMember, SubRabbleAdmin, Post, Comment, Conversation, ConversationMember, ConversationMessage
 
-# Register your models here.
+admin.site.register(Community)
+admin.site.register(CommunityMember)
+admin.site.register(SubRabble)
+admin.site.register(SubRabbleMember)
+admin.site.register(SubRabbleAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Conversation)
+admin.site.register(ConversationMember)
+admin.site.register(ConversationMessage)
